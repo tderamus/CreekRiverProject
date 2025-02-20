0ea17522e2b5d511c0467303a7bdc5c099effe8f
tderamus <deramust@gmail.com>
2025-02-19 21:09:07 -0600

    added api to get all campsites and campsites by id to programs.cs file

--- Program.cs
+++ Program.cs
@@ -6,8 +6,9 @@ using Microsoft.AspNetCore.Http.Json;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // allows passing datetimes without time zone data 
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
@@ -27,14 +28,37 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+// API CALLS TO GET DATABASE DATA
+
+// GET ALL CAMPSITES
 app.MapGet("/api/campsites", (CreekRiverDbContext db) =>
 {
-    return db.Campsites.ToList();
+    if (db.Campsites == null)
+    {
+        return Results.NotFound();
+    }
+    var campsites = db.Campsites.Include(c => c.CampsiteType).ToList();
+    return Results.Ok(campsites);
 });
 
+// GET CAMPSITE BY ID
+app.MapGet("/api/campsites/{id}", (CreekRiverDbContext db, int id) =>
+{
+    if (db.Campsites == null)
+    {
+        return Results.NotFound();
+    }   
+    var campsite = db.Campsites.Include(c => c.CampsiteType).SingleOrDefault(c => c.Id == id);
+    if (campsite == null)
+    {
+        return Results.NotFound();
+    }
+    return Results.Ok(campsite);
+});
 app.Run();
